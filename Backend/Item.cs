ef1c7406658c263336fe913dc6394e310564f0cd
jp811 <jp811@JP-MPOWERHOUSE>
2022-04-08 23:50:51 -0400

    Add project files.

---
+++ Backend/Item.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    internal class Item
+    {
+        private string _name;
+        private string _description;
+        private string _imageURL;
+        private double _price;
+
+        public string[] getItemInfo()
+        {
+            //create list to hold return array
+            List<string> items = new List<string>();
+
+            items.Add(_name);
+            items.Add(_description);
+            items.Add(_imageURL);
+            items.Add()
+
+            //return array
+            return items.ToArray();
+        }
+    }
+}
+//owo~ //mkvmkvdkl
---
+++ Backend/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    internal class Menu
+    {
+        private List<Item> _menuItems;
+        private List<Enhancment> _enhancments;
+
+        public List<Item> MenuItems
+        {
+            get { return _menuItems; }
+        }
+        public List<Enhancment> Enhancments
+        {
+            
+            get { return _enhancments; }
+        }
+
+
+}
